e6d1ed70376ac70ed1ae29d292fe2440d5c9ff92
ayesparshh <sparshgupta1001@gmail.com>
2024-07-17 03:03:03 +0530

    Shubharambh

---
+++ contracts/SwapItFactory.sol
@@ -0,0 +1,4 @@
+pragma solidity >=0.5.16;
+
+import './interfaces/ISwapItFactory.sol';
+import './SwapItPair.sol';
---
+++ contracts/SwapItPair.sol
@@ -0,0 +1,65 @@
+pragma solidity >=0.5.0;
+
+import './interfaces/ISwapItFactory.sol';
+import './SwapItERC20.sol';
+import './libraries/Math.sol';
+import './libraries/UQ112x112.sol';
+import './interfaces/ISwapItCallee.sol';
+import './interfaces/ISwapItPair.sol';
+import './interfaces/IERC20.sol';
+
+contract SwapItPair is ISwapItPair, SwapItERC20 {
+    using UQ112x112 for uint224;
+    using SafeMath for uint;
+
+    uint public constant MINIMUM_LIQUIDITY = 10**3;
+    bytes4 private constant SELECTOR = bytes4(keccak256(bytes('transfer(address,uint256)')));
+
+    address public factory;
+    address public token0;
+    address public token1;
+
+    uint112 private reserve0;           // uses single storage slot, accessible via getReserves
+    uint112 private reserve1;           // uses single storage slot, accessible via getReserves
+    uint32  private blockTimestampLast;  // uses single storage slot, accessible via getReserves
+
+    uint public price0CumulativeLast;
+    uint public price1CumulativeLast;
+    uint public kLast; // reserve0 * reserve1, as of immediately after the most recent liquidity event
+
+    uint private unlocked = 1;
+    modifier lock() {
+        require(unlocked == 1, 'SwapIt: LOCKED');
+        unlocked = 0;
+        _;
+        unlocked = 1;
+    }
+
+    function getReserves() public view returns (uint112 _reserve0, uint112 _reserve1, uint32 _blockTimestampLast) {
+        _reserve0 = reserve0;
+        _reserve1 = reserve1;
+        _blockTimestampLast = blockTimestampLast;
+    }
+
+    function _safeTransfer(address token, address to, uint value) private {
+        (bool success, bytes memory data) = token.call(abi.encodeWithSelector(SELECTOR, to, value));
+        require(success && (data.length == 0 || abi.decode(data, (bool))), 'SwapIt: TRANSFER_FAILED');
+    }
+
+    event Mint(address indexed sender, uint amount0, uint amount1);
+    event Burn(address indexed sender, uint amount0, uint amount1, address indexed to);
+    event Swap(
+        address indexed sender,
+        uint amount0In,
+        uint amount1In,
+        uint amount0Out,
+        uint amount1Out,
+        address indexed to
+    );
+
+    event Sync(uint112 reserve0, uint112 reserve1);
+
+    constructor() public {
+        factory = msg.sender;
+    }
+}
